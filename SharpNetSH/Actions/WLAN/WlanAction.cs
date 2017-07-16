4f049bc6735deb0cfabe16d435cc352f4a20a699
TotPe Ro <www.totpe.ro@gmail.com>
2017-07-16 14:02:00 +0300

    downgrade c# 6.0

--- SharpNetSH/Actions/WLAN/WlanAction.cs
+++ SharpNetSH/Actions/WLAN/WlanAction.cs
@@ -13,7 +13,10 @@ namespace Ignite.SharpNetSH.WLAN
 
         }
 
-        public string ActionName => "wlan";
+        public string ActionName
+        {
+            get { return "wlan"; }
+        }
 
         public void Initialize(string priorText, IExecutionHarness harness)
         {
