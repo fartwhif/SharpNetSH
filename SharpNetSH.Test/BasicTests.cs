825c5e9421273468d96f7684647acb75041169cc
ulyanov yuriy <ulyanov.yuriy@live.com>
2019-02-12 00:33:00 +0300

    basic test

---
+++ SharpNetSH.Test/BasicTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ignite.SharpNetSH.Test
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void FullUrlAclTest()
+        {
+            var testUrl = "http://localhost:8030/test";
+            var testUser = Environment.UserDomainName + "\\" + Environment.UserName;
+
+            var cmd = new CommandLineHarness();
+            var netsh = new NetSH(
+                cmd);
+            var http = netsh.Http;
+
+            var showResponse = http.Show.UrlAcl();
+            var addResponse = http.Add.UrlAcl(url: testUrl, user: testUser, sddl: null);
+        }
+    }
+}
