8be4e476a38e0cc7fd458b293c3502db36550f09
ulyanov yuriy <ulyanov.yuriy@live.com>
2019-02-12 22:18:31 +0300

    full http urlacl test

--- SharpNetSH.Test/BasicTests.cs
+++ SharpNetSH.Test/BasicTests.cs
@@ -12,13 +12,36 @@ namespace SharpNetSH.Test
             var testUrl = "http://localhost:8030/test/";
             var testUser = Environment.UserDomainName + "\\" + Environment.UserName;
 
-            var cmd = new CommandLineHarness();
-            var netsh = new NetSH(
-                cmd);
-            var http = netsh.Http;
+            var http = NetSH.CMD.Http;
 
-            var showResponse = http.Show.UrlAcl(testUrl);
-            var addResponse = http.Add.UrlAcl(url: testUrl, user: testUser, sddl: null);
+            var delete = http.Delete.UrlAcl(url: testUrl);
+            Assert.AreNotEqual(0, delete.ExitCode, delete.Response);
+
+            var show = http.Show.UrlAcl(testUrl);
+            Assert.AreEqual(0, show.ExitCode, show.Response);
+            var tree = show.ResponseObject as Tree;
+            Assert.IsNotNull(tree, show.Response);
+            Assert.AreEqual(0, tree.Children.Count, show.Response);
+
+            var add = http.Add.UrlAcl(url: testUrl, user: testUser, sddl: null);
+            Assert.AreEqual(0, add.ExitCode, add.Response);
+
+            add = http.Add.UrlAcl(url: testUrl, user: testUser, sddl: null);
+            Assert.AreNotEqual(0, add.ExitCode, add.Response);
+
+            show = http.Show.UrlAcl(testUrl);
+            Assert.AreEqual(0, show.ExitCode, show.Response);
+            tree = show.ResponseObject as Tree;
+            Assert.IsNotNull(tree, show.Response);
+
+            var urlNode = tree.Children[0];
+            Assert.AreEqual(testUrl, urlNode.Value, show.Response);
+
+            var userNode = urlNode.Children[0];
+            Assert.AreEqual(testUser, userNode.Value);
+
+            delete = http.Delete.UrlAcl(url: testUrl);
+            Assert.AreEqual(0, delete.ExitCode, delete.Response);
         }
     }
 }
