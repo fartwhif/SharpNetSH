85a88beeac59d26ed1319fac6cd41f241deb7cd7
somoene <email@place.com>
2024-02-13 01:11:57 -0600

    Fix incorrect response type

--- SharpNetSH/Actions/INTERFACE/PORTPROXY/IShowAction.cs
+++ SharpNetSH/Actions/INTERFACE/PORTPROXY/IShowAction.cs
@@ -6,23 +6,23 @@ namespace SharpNetSH.Actions.INTERFACE.PORTPROXY
     public interface IShowAction
     {
         [MethodName("all")]
-        [ResponseProcessor(typeof(HeaderedBlockProcessor), @":\s+")]
+        [ResponseProcessor(typeof(StandardResponse))]
         IResponse all();
 
         [MethodName("v4tov4")]
-        [ResponseProcessor(typeof(HeaderedBlockProcessor), @":\s+")]
+        [ResponseProcessor(typeof(StandardResponse))]
         IResponse v4tov4();
 
         [MethodName("v4tov6")]
-        [ResponseProcessor(typeof(HeaderedBlockProcessor), @":\s+")]
+        [ResponseProcessor(typeof(StandardResponse))]
         IResponse v4tov6();
 
         [MethodName("v6tov4")]
-        [ResponseProcessor(typeof(HeaderedBlockProcessor), @":\s+")]
+        [ResponseProcessor(typeof(StandardResponse))]
         IResponse v6tov4();
 
         [MethodName("v6tov6")]
-        [ResponseProcessor(typeof(HeaderedBlockProcessor), @":\s+")]
+        [ResponseProcessor(typeof(StandardResponse))]
         IResponse v6tov6();
     }
 }
